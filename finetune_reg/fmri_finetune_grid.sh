7e58321bfa2815cbde45fbdcbcecfaebdb71a5fd
Farzaneh-Taleb <farzanehtaleb11@gmail.com>
2025-08-26 11:37:00 +0200

    grids

--- finetune_reg/fmri_finetune_grid.sh
+++ finetune_reg/fmri_finetune_grid.sh
@@ -15,10 +15,10 @@ models=(
   "HUBioDataLab/SELFormer"
   "jonghyunlee/ChemBERT_ChEMBL_pretrained"
 )
-behavior_embeddings=("" "intensity" "pleasantness" "intensity, pleasantness")       # empty string => use default behavior columns inside your script
-unfreeze_layers=(1 2 3 4 5 6 -1)
+behavior_embeddings=("" "intensity" "pleasantness")       # empty string => use default behavior columns inside your script
+unfreeze_layers=(1 2 -1)
 lrs=(1e-5 2e-5 3e-5 4e-5 5e-5)
 weight_decays=(0.0)
-batch_sizes=(8 16)
+batch_sizes=(16)
 OUT_DIR="Aug26"
-EPOCHS=40
+EPOCHS=20
