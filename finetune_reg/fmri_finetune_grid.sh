3f882cd5c1cc5072cb27571c7c4795d3fb4f97d9
Farzaneh-Taleb <farzanehtaleb11@gmail.com>
2025-08-26 10:58:16 +0200

    grif files added

---
+++ finetune_reg/fmri_finetune_grid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# grid.sh – defines the experiment grid
+
+# Datasets to iterate over
+datasets=(
+  "sagar2023"
+)
+
+# Subjects, folds, and model/hparam grid
+subjects=(1 2 3)
+n_folds=(5)
+models=(
+  "ibm/MoLFormer-XL-both-10pct"
+  "seyonec/ChemBERTa-zinc-base-v1"
+  "HUBioDataLab/SELFormer"
+  "jonghyunlee/ChemBERT_ChEMBL_pretrained"
+)
+behavior_embeddings=("" "intensity" "pleasantness" "intensity, pleasantness")       # empty string => use default behavior columns inside your script
+unfreeze_layers=(1 2 3 4 5 6 -1)
+lrs=(1e-5 2e-5 3e-5 4e-5 5e-5)
+weight_decays=(0.0)
+batch_sizes=(8 16)
+OUT_DIR="Aug26"
+EPOCHS=40
---
+++ finetune_reg/fmri_grid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# FMRI grid (minimal: no nc_mask, func_mask, read_style, num_train_epochs, unfreeze_layers)
+
+datasets=(sagar2023)
+
+subjects=(1 2 3)
+folds=(5)
+n_components=("None" 30)
+
+models=("MoLFormer-XL-both-10pct" "ChemBERTa-zinc-base-v1" "SELFormer" "ChemBERT_ChEMBL_pretrained")
+
+rois=("PirF" "PirT" "AMY" "OFC")
+trs=(0 1 2 3 4 5 -1)
+z_scores=(True)
+OUT_DIR="Aug25fmri"
